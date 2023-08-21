38dc2e6f5fe6cb65b9141838102c5bec5a5144b6
Edward Shanahan <edwardshanahan357@gmail.com>
2023-08-21 16:52:54 +0100

    Add ContentAPI Hookes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,14 @@
 import css from "./App.module.css";
 import SideBar from "./components/Sidebar";
 import NavBarForm from "./components/NavBarForm";
-import ContentAPI from "./components/ContentAPI";
+import ContentAPIHooks from "./components/ContentAPIHooks";
 
 function App() {
   return (
     <div className={css.App}>
       <SideBar />
       <NavBarForm />
-      <ContentAPI />
+      <ContentAPIHooks />
     </div>
   );
 }
